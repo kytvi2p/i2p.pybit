bf0fc89001bf30953579e81dd4274a098d46debd
BlubMail <BlubMail@mail.i2p>
2009-09-16 16:56:42 +0000

    0.2.3 - 16.09.2009 ----------------------- Blub: - fixed bug in "Bittorrent.ConnectionListener": The listening socket wasn't closed when terminating PyBit (harmless because it was implicitly closed but explicit is better) - fixed bug in "Config": callbacks were called with encoded option values (essentially strings) instead of the real value - renamed "Logger.py" to "LogController.py": The class is called "LogController", so the file should be called the same. - renamed "HttpRequest" to "HttpResponseParser": More consistent that way, after all it parses responses, not requests. - improved "HttpRequest" slightly: Header key preference was backwards in one case (content-length / transfer-encoding), other small changes - updated PySamLib from v0.2.5 to v0.2.6

--- src/PyBit/PyBit.py
+++ src/PyBit/PyBit.py
@@ -18,7 +18,7 @@ You should have received a copy of the GNU General Public License
 along with PyBit.  If not, see <http://www.gnu.org/licenses/>.
 """
 
-VERSION = '0.2.2'
+VERSION = '0.2.3'
 
 
 ##builtin
